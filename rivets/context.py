f82e2583b194aadc15361554740114e1fcbc82ba
OiNutter <willmckenzie@oinutter.co.uk>
2012-05-01 08:23:31 +0100

    Added processor and error modules. Finished processing. Tidied indentation in context

--- rivets/context.py
+++ rivets/context.py
@@ -1,5 +1,9 @@
 import os
 import utils
+import base64
+import re
+import urllib
+import errors
 
 ''' `Context` provides helper methods to all `Tilt` processors. They
     are typically accessed by ERB templates. You can mix in custom
@@ -17,190 +21,185 @@ import utils
 '''
   
 class Context(object):
-    
-    environment
-    pathname
-    _required_paths
-    _stubbed_assets
-    _dependency_paths
-    _dependency_assets
-    __LINE__
 
-    def __init__(self,environment, logical_path, pathname):
-      self.environment  = environment
-      self._logical_path = logical_path
-      self.pathname     = pathname
-      self.__LINE__     = None
+  environment
+  pathname
+  _required_paths
+  _stubbed_assets
+  _dependency_paths
+  _dependency_assets
+  LINE
 
-      self._required_paths    = []
-      self._stubbed_assets    = set()
-      self._dependency_paths  = set()
-      self._dependency_assets = set([str(pathname)])
+  def __init__(self,environment, logical_path, pathname):
+    self.environment  = environment
+    self._logical_path = logical_path
+    self.pathname     = pathname
+    self.LINE     = None
+
+    self._required_paths    = []
+    self._stubbed_assets    = set()
+    self._dependency_paths  = set()
+    self._dependency_assets = set([str(pathname)])
  
-    def root_path(self):
-      ''' Returns the environment path that contains the file.
+  def root_path(self):
+    ''' Returns the environment path that contains the file.
           
-          If `app/javascripts` and `app/stylesheets` are in your path, and
-          current file is `app/javascripts/foo/bar.js`, `root_path` would
-          return `app/javascripts`.
-      '''
-      root_path = None
-      for path in self.environment.paths:
-        match = re.match(path,self.pathname)
-        if match:
-          root_path = re.sub(match[0],'',self.pathname)
+        If `app/javascripts` and `app/stylesheets` are in your path, and
+        current file is `app/javascripts/foo/bar.js`, `root_path` would
+        return `app/javascripts`.
+    '''
+    root_path = None
+    for path in self.environment.paths:
+      match = re.match(path,self.pathname)
+      if match:
+        root_path = re.sub(match[0],'',self.pathname)
 
-      return root_path
+    return root_path
 
-    def logical_path(self):
-      ''' Returns logical path without any file extensions.
+  def logical_path(self):
+    ''' Returns logical path without any file extensions.
           
-          'app/javascripts/application.js'
-          # => 'application'
-      '''
-      filename, ext = os.path.splitext(self._logical_path_)
-      return self._logical_path.rstrip(ext)
+      'app/javascripts/application.js'
+         # => 'application'
+    '''
+    filename, ext = os.path.splitext(self._logical_path_)
+    return self._logical_path.rstrip(ext)
 
-    def content_type(self):
-      ''' Returns content type of file
+  def content_type(self):
+    ''' Returns content type of file
     
           'application/javascript'
           # =>'text/css'
-      '''
-      return environment.content_type_of(self.pathname)
-
-    def resolve(path, options = {}, block=None):
-      ''' Given a logical path, `resolve` will find and return the fully
-          expanded path. Relative paths will also be resolved. An optional
-          `:content_type` restriction can be supplied to restrict the
-          search.
+    '''
+    return environment.content_type_of(self.pathname)
+  
+  def resolve(self,path, options = {}, block=None):
+    ''' Given a logical path, `resolve` will find and return the fully
+        expanded path. Relative paths will also be resolved. An optional
+        `:content_type` restriction can be supplied to restrict the
+        search.
     
-          resolve("foo.js")
-          # => "/path/to/app/javascripts/foo.js"
+        resolve("foo.js")
+        # => "/path/to/app/javascripts/foo.js"
     
-          resolve("./bar.js")
-          # => "/path/to/app/javascripts/bar.js"
-      '''
-      return os.path.abspath(path)
+        resolve("./bar.js")
+        # => "/path/to/app/javascripts/bar.js"
+    '''
+    return os.path.abspath(path)
       
-    def depend_on(path):
-      ''' `depend_on` allows you to state a dependency on a file without
-          including it.
+  def depend_on(self,path):
+    ''' `depend_on` allows you to state a dependency on a file without
+        including it.
           
-          This is used for caching purposes. Any changes made to
-          the dependency file with invalidate the cache of the
-          source file.
-      '''
-      self._dependency_paths.append(str(self.resolve(path)))
+        This is used for caching purposes. Any changes made to
+        the dependency file with invalidate the cache of the
+        source file.
+    '''
+    self._dependency_paths.append(str(self.resolve(path)))
+  
+  def depend_on_asset(self,path):
+    ''' `depend_on_asset` allows you to state an asset dependency
+        without including it.
 
-    def depend_on_asset(path):
-      ''' `depend_on_asset` allows you to state an asset dependency
-          without including it.
-        
-          This is used for caching purposes. Any changes that would
-          invalidate the dependency asset will invalidate the source
-          file. Unlike `depend_on`, this will include recursively include
-          the target asset's dependencies.
-      '''
-      filename = str(self.resolve(path))
-      self._dependency_assets.append(filename)
+        This is used for caching purposes. Any changes that would
+        invalidate the dependency asset will invalidate the source
+        file. Unlike `depend_on`, this will include recursively include
+        the target asset's dependencies.
+    '''
+    filename = str(self.resolve(path))
+    self._dependency_assets.append(filename)
 
-    def require_asset(path):
-      ''' `require_asset` declares `path` as a dependency of the file. The
-          dependency will be inserted before the file and will only be
-          included once.
-      '''
-      pathname = self.resolve(path)
-      depend_on_asset(pathname)
-      self._required_paths.append(pathname)
+  def require_asset(self,path):
+    ''' `require_asset` declares `path` as a dependency of the file. The
+        dependency will be inserted before the file and will only be
+        included once.
+    '''
+    pathname = self.resolve(path)
+    self.depend_on_asset(pathname)
+    self._required_paths.append(pathname)
 
-    def stub_asset(path):
-      ''' `stub_asset` blacklists `path` from being included in the bundle.
-          `path` must be an asset which may or may not already be included
-          in the bundle.
-      '''
-      self._stubbed_assets.append(str(self.resolve(path))
+  def stub_asset(self,path):
+    ''' `stub_asset` blacklists `path` from being included in the bundle.
+        `path` must be an asset which may or may not already be included
+        in the bundle.
+    '''
+    self._stubbed_assets.append(str(self.resolve(path)))
       
-  def is_asset_requirable(path):
+  def is_asset_requirable(self,path):
     ''' Tests if target path is able to be safely required into the
         current concatenation.
     '''
     pathname = self.resolve(path)
     content_type = self.environment.content_type_of(pathname)
-    stat = environment.stat(path)
+    stat = self.environment.stat(path)
     
-    if not stat
-    return false unless stat && stat.file?
-      self.content_type.nil? || self.content_type == content_type
-    end
+    if not stat and not stat.is_file():
+      return False
 
-    # Reads `path` and runs processors on the file.
-    #
-    # This allows you to capture the result of an asset and include it
-    # directly in another.
-    #
-    #     <%= evaluate "bar.js" %>
-    #
-    def evaluate(path, options = {})
-      pathname   = resolve(path)
-      attributes = environment.attributes_for(pathname)
-      processors = options[:processors] || attributes.processors
+    return self.content_type is None or self.content_type == content_type
 
-      if options[:data]
-        result = options[:data]
-      else
-        if environment.respond_to?(:default_external_encoding)
-          mime_type = environment.mime_types(pathname.extname)
-          encoding  = environment.encoding_for_mime_type(mime_type)
-          result    = Sprockets::Utils.read_unicode(pathname, encoding)
-        else
-          result = Sprockets::Utils.read_unicode(pathname)
-        end
-      end
+  def evaluate(self,path, options = {}):
+    ''' Reads `path` and runs processors on the file.
+          
+        This allows you to capture the result of an asset and include it
+        directly in another.
+          
+             <%= evaluate "bar.js" %>
+        
+    '''
+    pathname   = self.resolve(path)
+    attributes = self.environment.attributes_for(pathname)
+    processors = options[:processors] or attributes.processors
 
-      processors.each do |processor|
-        begin
-          template = processor.new(pathname.to_s) { result }
-          result = template.render(self, {})
-        rescue Exception => e
-          annotate_exception! e
-          raise
-        end
-      end
+    if options['data']:
+      result = options['data']
+    else:
+      if has_attr(environment,'default_external_encoding') and callable(environment,'default_external_encoding'):
+        file,ext = os.path.splitext(pathname)
+        mime_type = environment.mime_types(ext)
+        encoding  = environment.encoding_for_mime_type(mime_type)
+        result    = utils.read_unicode(pathname, encoding)
+      else:
+        result = utils.read_unicode(pathname)
 
-      result
-    end
+    for processor in processors:
+      try:
+        template = processor(str(pathname),lambda x:result)
+        result = template.render(self, {})
+      except Exception, e:
+        e = self.annotate_exception(e)
+        raise Exception(e)
 
-    # Returns a Base64-encoded `data:` URI with the contents of the
-    # asset at the specified path, and marks that path as a dependency
-    # of the current file.
-    #
-    # Use `asset_data_uri` from ERB with CSS or JavaScript assets:
-    #
-    #     #logo { background: url(<%= asset_data_uri 'logo.png' %>) }
-    #
-    #     $('<img>').attr('src', '<%= asset_data_uri 'avatar.jpg' %>')
-    #
-    def asset_data_uri(path)
-      depend_on_asset(path)
-      asset  = environment.find_asset(path)
-      base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
-      "data:#{asset.content_type};base64,#{Rack::Utils.escape(base64)}"
-    end
+    return result
 
-    private
-      # Annotates exception backtrace with the original template that
-      # the exception was raised in.
-      def annotate_exception!(exception)
-        location = pathname.to_s
-        location << ":#{@__LINE__}" if @__LINE__
+  def asset_data_uri(self,path):
+    ''' Returns a Base64-encoded `data:` URI with the contents of the
+        asset at the specified path, and marks that path as a dependency
+        of the current file.
+      
+        Use `asset_data_uri` from ERB with CSS or JavaScript assets:
+        
+        #logo { background: url(<%= asset_data_uri 'logo.png' %>) }
+      
+        $('<img>').attr('src', '<%= asset_data_uri 'avatar.jpg' %>')
+    '''
+    self.depend_on_asset(path)
+    asset  = self.environment.find_asset(path)
+    base64 = re.sub(r"\s+", "",base64.b64encode(str(asset)))
+    return "data:%s;base64,%s" % (asset.content_type, urllib.quote_plus(base64))
 
-        exception.extend(Sprockets::EngineError)
-        exception.sprockets_annotation = "  (in #{location})"
-      end
+  def annotate_exception(self):
+    ''' Annotates exception backtrace with the original template that
+        the exception was raised in.
+    '''
+    location = str(pathname)
+    if self.LINE:
+      location += ":%d" % self.LINE
 
-      def logger
-        environment.logger
-      end
-  end
-end
+    exception = errors.EngineError(exception)
+    exception.sprockets_annotation = "  (in %s)" % location
+
+    return exception
+
+  def logger(self):
+    return self.environment.logger
--- rivets/processing.py
+++ rivets/processing.py
@@ -1,6 +1,8 @@
-import utils
 import engines
 import copy
+from processor import Processor
+import mime
+import rivets
 
 ''' `Processing` is an internal mixin whose public methods are exposed on
     the `Environment` and `Index` classes.
@@ -16,7 +18,7 @@ def format_extensions():
       format_extensions
       # => ['.js', '.css']
   '''
-  return trail.extensions - engines.engines.keys()
+  return rivets.trail.extensions - engines.engines.keys()
 
 def preprocessors(mime_type = None):
   ''' Returns an `Array` of `Processor` classes. If a `mime_type`
@@ -59,9 +61,9 @@ def register_preprocessor(mime_type, klass, block=None):
         register_preprocessor(lambda context, data: re.sub(...))
   '''
 
-  #if block:
-  #  name  = str(klass)
-  #  klass = block
+  if block:
+    name  = str(klass)
+    klass = Processor(name,block)
 
   _preprocessors[mime_type].append(klass)
 
@@ -75,9 +77,9 @@ def register_postprocessor(mime_type, klass, block = None):
         register_postprocessor(lambda context, data: re.sub(...))
   '''
 
-  #if block_given?
-  #      name  = str(klass)
-  #      klass = block
+  if block:
+    name  = str(klass)
+    klass = Processor(name,block)
 
   _postprocessors[mime_type].append(klass)
 
@@ -87,9 +89,9 @@ def unregister_preprocessor(mime_type, klass):
         unregister_preprocessor('text/css', rivets.directive_processor.DirectiveProcessor)
   '''
 
-  if isinstance(klass,string):
+  if isinstance(klass,'string'):
     for processor in _preprocessors:
-      if processor.__name__ == klass:
+      if processor.__name__ =='rivets.processor.Processor (%s)' % klass:
         klass = processor
         break
 
@@ -101,9 +103,9 @@ def unregister_postprocessor(mime_type, klass):
         unregister_postprocessor 'text/css', Sprockets::DirectiveProcessor
   '''
 
-  if isinstance(klass,string):
+  if isinstance(klass,'string'):
     for processor in _postprocessors:
-      if processor.__name__ == klass:
+      if processor.__name__ == 'rivets.processor.Processor (%s)' % klass:
         klass = processor
         break
 
@@ -133,9 +135,9 @@ def register_bundle_processor(mime_type, klass, block=None):
       
         register_bundle_processor(lambda context, data: re.sub(...))
   '''
-  #if block_given?
-  #      name  = str(klass)
-  #      klass = block
+  if block:
+    name  = str(klass)
+    klass = Processor(name,block)
 
   _bundleprocessors[mime_type].append(klass)
 
@@ -145,60 +147,60 @@ def unregister_bundle_processor(mime_type, klass):
         unregister_bundle_processor 'text/css', Sprockets::CharsetNormalizer
   '''
   
-  if isinstance(klass,string):
+  if isinstance(klass,'string'):
     for processor in _postprocessors:
-      if processor.__name__ == klass:
+      if processor.__name__ == 'rivets.processor.Processor (%s)' % klass:
         klass = processor
         break
 
-    _bundle_processors[mime_type].remove(klass)
+    _bundleprocessors[mime_type].remove(klass)
 
 def get_css_compressor():
   ''' Return CSS compressor or None if none is set '''
   compressor = None
   for processor in bundle_processors['text/css']:
-    if processor == css_compressor:
+    if processor.__name__ == 'rivets.processor.Processor (CSSCompressor)':
       compressor = processor
+  return compressor
     
 def set_css_compressor(compressor=None):
   ''' Assign a compressor to run on `text/css` assets.
 
       The compressor object must respond to `compress` or `compile`.
   '''
-  unregister_bundle_processor('text/css', css_compressor)
+  unregister_bundle_processor('text/css', 'CSSCompressor')
   if not compressor:
     return
 
-  register_bundle_processor('text/css', compressor.compress);
+  klass = Processor('CSSCompressor',lambda context,data:compressor.compress(data))
 
-def js_compressor(compressor=None):
-  bundle_processors('application/javascript').detect { |klass|
-        klass.respond_to?(:name) &&
-          klass.name == 'Sprockets::Processor (js_compressor)'
-      }
-    end
+  register_bundle_processor('text/css', klass)
 
-    # Assign a compressor to run on `application/javascript` assets.
-    #
-    # The compressor object must respond to `compress` or `compile`.
-    def js_compressor=(compressor)
-      unregister_bundle_processor 'application/javascript', :js_compressor
-      return unless compressor
+def get_js_compressor():
+  ''' Return JS compressor or None if none is set '''
+  compressor = None
+  for processor in bundle_processors['application/javascript']:
+    if processor.__name__ == 'rivets.processor.Processor (JSCompressor)':
+      compressor = processor
+  return compressor
+ 
+def set_js_compressor(compressor=None):
+  ''' Assign a compressor to run on `application/javascript` assets.
 
-      register_bundle_processor 'application/javascript', :js_compressor do |context, data|
-        compressor.compress(data)
-      end
-    end
+      The compressor object must respond to `compress` or `compile`.
+  '''
+  unregister_bundle_processor('application/javascript','JSCompressor')
+  if not compressor:
+    return
 
-    private
-      def add_engine_to_trail(ext, klass)
-        @trail.append_extension(ext.to_s)
+  klass = Processor('JSCompressor',lambda context,data:compressor.compress(data))
 
-        if klass.respond_to?(:default_mime_type) && klass.default_mime_type
-          if format_ext = extension_for_mime_type(klass.default_mime_type)
-            @trail.alias_extension(ext.to_s, format_ext)
-          end
-        end
-      end
-  end
-end
+  register_bundle_processor('application/javascript', klass)
+
+  def add_engine_to_trail(ext, klass):
+    rivets.trail.append_extension(str(ext))
+
+    if hasattr(klass,'default_mime_type') and callable(klass,'default_mime_type') and klass.default_mime_type():
+      format_ext = mime.extension_for_mime_type(klass.default_mime_type)
+      if format_ext:
+        return rivets.trail.alias_extension(str(ext), format_ext)
