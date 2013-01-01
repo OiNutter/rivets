15de07fd0cdde461e0d86485bcac3622aafc39cb
OiNutter <willmckenzie@oinutter.co.uk>
2013-01-01 23:32:27 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='Rivets',
+	  version='0.1',
+	  description='Python asset packaging system. Based on Sprockets ruby gem',
+	  author='Will McKenzie',
+	  author_email='will@oinutter.co.uk',
+	  packages=['rivets'],
+	  package_dir={'rivets': 'rivets'},
+	  requires=['crawl','shift']
+	  )
