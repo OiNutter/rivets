12918d4539509652c8d2d27fb387cbdcdfbf56e0
OiNutter <willmckenzie@oinutter.co.uk>
2013-01-13 23:14:01 +0000

    Expanding setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,23 @@ from distutils.core import setup
 
 setup(name='Rivets',
 	  version='0.2',
+	  url='https://github.com/OiNutter/rivets',
+	  download_url='https://github.com/OiNutter/rivets/tarball/master',
 	  description='Python asset packaging system. Based on Sprockets ruby gem',
 	  author='Will McKenzie',
 	  author_email='will@oinutter.co.uk',
 	  packages=['rivets'],
 	  package_dir={'rivets': 'rivets'},
-	  requires=['crawl','lean','uglipyjs','slimmer']
-	  )
+	  requires=['crawl','lean','uglipyjs','slimmer'],
+	  license='MIT License',
+	  classifiers=[
+	  		'Development Status :: 3 - Alpha',
+	  		'Environment :: Web Environment',
+	  		'Intended Audience :: Developers',
+	  		'License :: OSI Approved :: MIT License',
+	  		'Programming Language :: Python :: 2',
+        	'Programming Language :: Python :: 2.6',
+        	'Programming Language :: Python :: 2.7',
+        	'Programming Language :: JavaScript'
+	  ]
+)
